124357854baa0ebee746c1bca41b4943e9a8e747
Ryan Twohey <ryantwohey@Ryans-MBP-2.attlocal.net>
2022-08-19 10:27:48 -0400

    Succesfully navigate to Ionic HQ

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -9,4 +11,33 @@ export class HomePage {
 
   constructor() {}
 
+
+  navigate() {
+    const navigator = new LaunchNavigator()
+    let default_app = navigator.APP.GOOGLE_MAPS;
+    const address = {
+      name: '121 S Pinckney St, Madison WI',
+      Latitude: 43.07423923811246,
+      Longitude: -89.3809923212564
+    };
+
+    let options: LaunchNavigatorOptions = {
+      app: default_app
+    };
+    if (default_app == null) {
+      navigator.navigate( [address.Latitude, address.Longitude] ).then( (success:any) => {
+        console.log( 'success => ' + JSON.stringify( success ) );
+      }, (error:any) => {
+        console.log( 'error => ' + JSON.stringify( error ) );
+        console.log( 'Unable to launch navigation' );
+      });
+    } else {
+      navigator.navigate( [address.Latitude, address.Longitude], options).then( (success:any) => {
+        console.log( 'success => ' + JSON.stringify( success ) );
+      }, (error:any) => {
+        console.log( 'error => ' + JSON.stringify( error ) );
+        console.log( 'Unable to launch navigation' );
+      });
+    }
+  }
 }
